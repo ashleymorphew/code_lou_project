a13271103d67b1e9bcdcbd2dc06a6fbfd3d217ea
Ashley Morphew <amorphew93@gmail.com>
2022-03-15 18:54:05 -0400

    form validation added

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ function myFunction(x) {
     }
   }
 
+function validateForm() {
+  let name = document.forms["contactForm"]["name"].value;
+  if (name == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
+     
