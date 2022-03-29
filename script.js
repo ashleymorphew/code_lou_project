9859bceb3a222d7bf6e2fa1d3b933f2a8031d646
Ashley Morphew <amorphew93@gmail.com>
2022-03-28 21:15:52 -0400

    more form validation added

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
-function myFunction(x) {
+function myFunction() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
       x.style.display = "none";
-    } else {
+    } 
+    else {
       x.style.display = "block";
     }
   }
 
 function validateForm() {
   let name = document.forms["contactForm"]["name"].value;
-  if (name == "") {
-    alert("Name must be filled out");
+  let email = document.forms["contactForm"]["email"].value;
+  if (name == "" && email == "") {
+    alert("Form must be filled out!");
+    return false;
+  }
+  else if (name == "") {
+    alert("Name must be filled out!")
+    return false;
+  }
+  else if (email == "") {
+    alert("Email must be filled out!")
     return false;
   }
 }
-     
+
+
+
